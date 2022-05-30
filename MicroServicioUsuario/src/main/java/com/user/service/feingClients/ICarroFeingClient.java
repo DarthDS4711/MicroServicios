4443e64d16cd0a1489e3aeab50cbfa6d21d17fb1
DarthDS4711 <daniel.santiago@capitaltech.com>
2022-05-30 12:06:49 -0500

    gateway

--- MicroServicioUsuario/src/main/java/com/user/service/feingClients/ICarroFeingClient.java
+++ MicroServicioUsuario/src/main/java/com/user/service/feingClients/ICarroFeingClient.java
@@ -1,16 +1,14 @@
 package com.user.service.feingClients;
 
 import com.user.service.models.Car;
-import com.user.service.models.Moto;
 import java.util.List;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient(name = "carro-service", url="http://localhost:8082")
+@FeignClient(name = "carro-service")
 public interface ICarroFeingClient {
     
     @PostMapping("/carro")
